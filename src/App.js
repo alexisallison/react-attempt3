09fab8d7577b42f53789f1f3003d220c576f21c3
alexisallison <alexisallison1801@gmail.com>
2023-04-17 16:55:08 -0400

    added github link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
+      <footer>
+        <p>
+          This code is{" "}
+          <a href="https://github.com/alexisallison/react-attempt3.git">
+            Open-Sourced
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
